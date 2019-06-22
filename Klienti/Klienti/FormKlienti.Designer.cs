5247859b2696e0d4148fca25eb1f17fc03601627
Muhamed Zahiri <muhamedzahiri@gmail.com>
2019-06-22 11:07:39 +0200

    Klienti

---
+++ Klienti/Klienti/FormKlienti.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Klienti
+{
+    partial class FormaUP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormaUP));
+            this.GrBoxRegjistrohu = new System.Windows.Forms.GroupBox();
+            this.txtFjalkalimi = new System.Windows.Forms.TextBox();
+            this.btnRegjistrohu = new System.Windows.Forms.Button();
+            this.txtNofka = new System.Windows.Forms.TextBox();
+            this.txtEPosta = new System.Windows.Forms.TextBox();
+            this.txtPaga = new System.Windows.Forms.TextBox();
+            this.txtTitulli = new System.Windows.Forms.TextBox();
+            this.txtEmriMbiemri = new System.Windows.Forms.TextBox();
+            this.labelFjalkalimi = new System.Windows.Forms.Label();
+            this.labelNofka = new System.Windows.Forms.Label();
+            this.labelEPosta = new System.Windows.Forms.Label();
+            this.labelPaga = new System.Windows.Forms.Label();
+            this.labelTitulli = new System.Windows.Forms.Label();
+            this.labelEmriMbiemri = new System.Windows.Forms.Label();
+            this.GrBoxHyrja = new System.Windows.Forms.GroupBox();
+            this.btnFutu = new System.Windows.Forms.Button();
+            this.txtFjalkalimiHyrja = new System.Windows.Forms.TextBox();
+            this.txtNofkaHyrja = new System.Windows.Forms.TextBox();
+            this.labelFjalkalimiHyrja = new System.Windows.Forms.Label();
+            this.labelNofkaHyrja = new System.Windows.Forms.Label();
+            this.UpLogo = new System.Windows.Forms.PictureBox();
+            this.labelPergjigjiaServerit = new System.Windows.Forms.Label();
+            this.txtPergjigjiaServeri = new System.Windows.Forms.TextBox();
+            this.GrBoxRegjistrohu.SuspendLayout();
+            this.GrBoxHyrja.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UpLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GrBoxRegjistrohu
+            // 
+            this.GrBoxRegjistrohu.Controls.Add(this.txtFjalkalimi);
+            this.GrBoxRegjistrohu.Controls.Add(this.btnRegjistrohu);
+            this.GrBoxRegjistrohu.Controls.Add(this.txtNofka);
+            this.GrBoxRegjistrohu.Controls.Add(this.txtEPosta);
+            this.GrBoxRegjistrohu.Controls.Add(this.txtPaga);
+            this.GrBoxRegjistrohu.Controls.Add(this.txtTitulli);
+            this.GrBoxRegjistrohu.Controls.Add(this.txtEmriMbiemri);
+            this.GrBoxRegjistrohu.Controls.Add(this.labelFjalkalimi);
+            this.GrBoxRegjistrohu.Controls.Add(this.labelNofka);
+            this.GrBoxRegjistrohu.Controls.Add(this.labelEPosta);
+            this.GrBoxRegjistrohu.Controls.Add(this.labelPaga);
+            this.GrBoxRegjistrohu.Controls.Add(this.labelTitulli);
+            this.GrBoxRegjistrohu.Controls.Add(this.labelEmriMbiemri);
+            this.GrBoxRegjistrohu.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GrBoxRegjistrohu.Location = new System.Drawing.Point(0, 0);
+            this.GrBoxRegjistrohu.Name = "GrBoxRegjistrohu";
+            this.GrBoxRegjistrohu.Size = new System.Drawing.Size(254, 214);
+            this.GrBoxRegjistrohu.TabIndex = 0;
+            this.GrBoxRegjistrohu.TabStop = false;
+            this.GrBoxRegjistrohu.Text = "Regjistrohu";
+            // 
+            // txtFjalkalimi
+            // 
+            this.txtFjalkalimi.Location = new System.Drawing.Point(129, 161);
+            this.txtFjalkalimi.Name = "txtFjalkalimi";
+            this.txtFjalkalimi.Size = new System.Drawing.Size(116, 20);
+            this.txtFjalkalimi.TabIndex = 2;
+            // 
+            // btnRegjistrohu
+            // 
+            this.btnRegjistrohu.Location = new System.Drawing.Point(163, 187);
+            this.btnRegjistrohu.Name = "btnRegjistrohu";
+            this.btnRegjistrohu.Size = new System.Drawing.Size(83, 23);
+            this.btnRegjistrohu.TabIndex = 3;
+            this.btnRegjistrohu.Text = "Regjistrohu";
+            this.btnRegjistrohu.UseVisualStyleBackColor = true;
+            this.btnRegjistrohu.Click += new System.EventHandler(this.BtnRegjistrohu_Click);
+            // 
+            // txtNofka
+            // 
+            this.txtNofka.Location = new System.Drawing.Point(129, 135);
+            this.txtNofka.Name = "txtNofka";
+            this.txtNofka.Size = new System.Drawing.Size(116, 20);
+            this.txtNofka.TabIndex = 3;
+            // 
+            // txtEPosta
+            // 
+            this.txtEPosta.Location = new System.Drawing.Point(129, 109);
+            this.txtEPosta.Name = "txtEPosta";
+            this.txtEPosta.Size = new System.Drawing.Size(116, 20);
+            this.txtEPosta.TabIndex = 4;
+            // 
+            // txtPaga
+            // 
+            this.txtPaga.Location = new System.Drawing.Point(129, 83);
+            this.txtPaga.Name = "txtPaga";
+            this.txtPaga.Size = new System.Drawing.Size(116, 20);
+            this.txtPaga.TabIndex = 5;
+            // 
+            // txtTitulli
+            // 
+            this.txtTitulli.Location = new System.Drawing.Point(129, 57);
+            this.txtTitulli.Name = "txtTitulli";
+            this.txtTitulli.Size = new System.Drawing.Size(116, 20);
+            this.txtTitulli.TabIndex = 6;
+            // 
+            // txtEmriMbiemri
+            // 
+            this.txtEmriMbiemri.BackColor = System.Drawing.Color.White;
+            this.txtEmriMbiemri.Location = new System.Drawing.Point(129, 31);
+            this.txtEmriMbiemri.Name = "txtEmriMbiemri";
+            this.txtEmriMbiemri.Size = new System.Drawing.Size(116, 20);
+            this.txtEmriMbiemri.TabIndex = 7;
+            // 
+            // labelFjalkalimi
+            // 
+            this.labelFjalkalimi.AutoSize = true;
+            this.labelFjalkalimi.Location = new System.Drawing.Point(8, 167);
+            this.labelFjalkalimi.Name = "labelFjalkalimi";
+            this.labelFjalkalimi.Size = new System.Drawing.Size(53, 14);
+            this.labelFjalkalimi.TabIndex = 6;
+            this.labelFjalkalimi.Text = "Fjalkalimi";
+            // 
+            // labelNofka
+            // 
+            this.labelNofka.AutoSize = true;
+            this.labelNofka.Location = new System.Drawing.Point(7, 141);
+            this.labelNofka.Name = "labelNofka";
+            this.labelNofka.Size = new System.Drawing.Size(36, 14);
+            this.labelNofka.TabIndex = 5;
+            this.labelNofka.Text = "Nofka";
+            // 
+            // labelEPosta
+            // 
+            this.labelEPosta.AutoSize = true;
+            this.labelEPosta.Location = new System.Drawing.Point(7, 116);
+            this.labelEPosta.Name = "labelEPosta";
+            this.labelEPosta.Size = new System.Drawing.Size(44, 14);
+            this.labelEPosta.TabIndex = 4;
+            this.labelEPosta.Text = "E-Posta";
+            // 
+            // labelPaga
+            // 
+            this.labelPaga.AutoSize = true;
+            this.labelPaga.Location = new System.Drawing.Point(8, 89);
+            this.labelPaga.Name = "labelPaga";
+            this.labelPaga.Size = new System.Drawing.Size(29, 14);
+            this.labelPaga.TabIndex = 3;
+            this.labelPaga.Text = "Paga";
+            // 
+            // labelTitulli
+            // 
+            this.labelTitulli.AutoSize = true;
+            this.labelTitulli.Location = new System.Drawing.Point(8, 63);
+            this.labelTitulli.Name = "labelTitulli";
+            this.labelTitulli.Size = new System.Drawing.Size(36, 14);
+            this.labelTitulli.TabIndex = 2;
+            this.labelTitulli.Text = "Titulli";
+            // 
+            // labelEmriMbiemri
+            // 
+            this.labelEmriMbiemri.AutoSize = true;
+            this.labelEmriMbiemri.Location = new System.Drawing.Point(7, 37);
+            this.labelEmriMbiemri.Name = "labelEmriMbiemri";
+            this.labelEmriMbiemri.Size = new System.Drawing.Size(91, 14);
+            this.labelEmriMbiemri.TabIndex = 1;
+            this.labelEmriMbiemri.Text = "Emri dhe Mbiemri";
+            // 
+            // GrBoxHyrja
+            // 
+            this.GrBoxHyrja.Controls.Add(this.btnFutu);
+            this.GrBoxHyrja.Controls.Add(this.txtFjalkalimiHyrja);
+            this.GrBoxHyrja.Controls.Add(this.txtNofkaHyrja);
+            this.GrBoxHyrja.Controls.Add(this.labelFjalkalimiHyrja);
+            this.GrBoxHyrja.Controls.Add(this.labelNofkaHyrja);
+            this.GrBoxHyrja.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GrBoxHyrja.Location = new System.Drawing.Point(261, 116);
+            this.GrBoxHyrja.Name = "GrBoxHyrja";
+            this.GrBoxHyrja.Size = new System.Drawing.Size(203, 98);
+            this.GrBoxHyrja.TabIndex = 0;
+            this.GrBoxHyrja.TabStop = false;
+            this.GrBoxHyrja.Text = "Hyrja";
+            // 
+            // btnFutu
+            // 
+            this.btnFutu.Location = new System.Drawing.Point(138, 71);
+            this.btnFutu.Name = "btnFutu";
+            this.btnFutu.Size = new System.Drawing.Size(55, 23);
+            this.btnFutu.TabIndex = 4;
+            this.btnFutu.Text = "Futu";
+            this.btnFutu.UseVisualStyleBackColor = true;
+            this.btnFutu.Click += new System.EventHandler(this.BtnFutu_Click);
+            // 
+            // txtFjalkalimiHyrja
+            // 
+            this.txtFjalkalimiHyrja.Location = new System.Drawing.Point(76, 45);
+            this.txtFjalkalimiHyrja.Name = "txtFjalkalimiHyrja";
+            this.txtFjalkalimiHyrja.Size = new System.Drawing.Size(116, 20);
+            this.txtFjalkalimiHyrja.TabIndex = 2;
+            // 
+            // txtNofkaHyrja
+            // 
+            this.txtNofkaHyrja.Location = new System.Drawing.Point(76, 19);
+            this.txtNofkaHyrja.Name = "txtNofkaHyrja";
+            this.txtNofkaHyrja.Size = new System.Drawing.Size(116, 20);
+            this.txtNofkaHyrja.TabIndex = 1;
+            // 
+            // labelFjalkalimiHyrja
+            // 
+            this.labelFjalkalimiHyrja.AutoSize = true;
+            this.labelFjalkalimiHyrja.Location = new System.Drawing.Point(7, 51);
+            this.labelFjalkalimiHyrja.Name = "labelFjalkalimiHyrja";
+            this.labelFjalkalimiHyrja.Size = new System.Drawing.Size(53, 14);
+            this.labelFjalkalimiHyrja.TabIndex = 1;
+            this.labelFjalkalimiHyrja.Text = "Fjalkalimi";
+            // 
+            // labelNofkaHyrja
+            // 
+            this.labelNofkaHyrja.AutoSize = true;
+            this.labelNofkaHyrja.Location = new System.Drawing.Point(7, 25);
+            this.labelNofkaHyrja.Name = "labelNofkaHyrja";
+            this.labelNofkaHyrja.Size = new System.Drawing.Size(36, 14);
+            this.labelNofkaHyrja.TabIndex = 0;
+            this.labelNofkaHyrja.Text = "Nofka";
+            // 
+            // UpLogo
+            // 
+            this.UpLogo.Image = ((System.Drawing.Image)(resources.GetObject("UpLogo.Image")));
+            this.UpLogo.Location = new System.Drawing.Point(327, 21);
+            this.UpLogo.Name = "UpLogo";
+            this.UpLogo.Size = new System.Drawing.Size(72, 60);
+            this.UpLogo.TabIndex = 0;
+            this.UpLogo.TabStop = false;
+            // 
+            // labelPergjigjiaServerit
+            // 
+            this.labelPergjigjiaServerit.AutoSize = true;
+            this.labelPergjigjiaServerit.Location = new System.Drawing.Point(7, 230);
+            this.labelPergjigjiaServerit.Name = "labelPergjigjiaServerit";
+            this.labelPergjigjiaServerit.Size = new System.Drawing.Size(59, 13);
+            this.labelPergjigjiaServerit.TabIndex = 1;
+            this.labelPergjigjiaServerit.Text = "Përgjigjia";
+            // 
+            // txtPergjigjiaServeri
+            // 
+            this.txtPergjigjiaServeri.Location = new System.Drawing.Point(9, 246);
+            this.txtPergjigjiaServeri.Multiline = true;
+            this.txtPergjigjiaServeri.Name = "txtPergjigjiaServeri";
+            this.txtPergjigjiaServeri.ReadOnly = true;
+            this.txtPergjigjiaServeri.Size = new System.Drawing.Size(452, 57);
+            this.txtPergjigjiaServeri.TabIndex = 2;
+            // 
+            // FormaUP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(476, 314);
+            this.Controls.Add(this.txtPergjigjiaServeri);
+            this.Controls.Add(this.labelPergjigjiaServerit);
+            this.Controls.Add(this.GrBoxHyrja);
+            this.Controls.Add(this.UpLogo);
+            this.Controls.Add(this.GrBoxRegjistrohu);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormaUP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mësimdhënësi i UP-së";
+            this.GrBoxRegjistrohu.ResumeLayout(false);
+            this.GrBoxRegjistrohu.PerformLayout();
+            this.GrBoxHyrja.ResumeLayout(false);
+            this.GrBoxHyrja.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.UpLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GrBoxRegjistrohu;
+        private System.Windows.Forms.Label labelFjalkalimi;
+        private System.Windows.Forms.Label labelNofka;
+        private System.Windows.Forms.Label labelEPosta;
+        private System.Windows.Forms.Label labelPaga;
+        private System.Windows.Forms.Label labelTitulli;
+        private System.Windows.Forms.Label labelEmriMbiemri;
+        private System.Windows.Forms.GroupBox GrBoxHyrja;
+        private System.Windows.Forms.PictureBox UpLogo;
+        private System.Windows.Forms.TextBox txtFjalkalimi;
+        private System.Windows.Forms.TextBox txtNofka;
+        private System.Windows.Forms.TextBox txtEPosta;
+        private System.Windows.Forms.TextBox txtPaga;
+        private System.Windows.Forms.TextBox txtTitulli;
+        private System.Windows.Forms.TextBox txtEmriMbiemri;
+        private System.Windows.Forms.TextBox txtFjalkalimiHyrja;
+        private System.Windows.Forms.TextBox txtNofkaHyrja;
+        private System.Windows.Forms.Label labelFjalkalimiHyrja;
+        private System.Windows.Forms.Label labelNofkaHyrja;
+        private System.Windows.Forms.Label labelPergjigjiaServerit;
+        private System.Windows.Forms.TextBox txtPergjigjiaServeri;
+        private System.Windows.Forms.Button btnRegjistrohu;
+        private System.Windows.Forms.Button btnFutu;
+    }
+}
+
---
+++ Klienti/Klienti/FormKlienti.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+using System.Security.Cryptography.Xml;
+using System.Xml.Linq;
+
+namespace Klienti
+{
+    public partial class FormaUP : Form
+    {
+
+
+        public static Socket soketKlienti;
+
+        public FormaUP()
+        {
+            InitializeComponent();
+            Lidhja();
+        }
+
+        public void Lidhja()
+        {
+
+            String IPAdresa = "127.0.0.1";
+            Int32 Porti = 1234;
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(IPAdresa), Porti);
+            soketKlienti = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                soketKlienti.Connect(ip);
+                txtPergjigjiaServeri.AppendText("Jeni lidhur në IP-në: " + IPAdresa + " dhe Portin: " + Porti + "\n");
+                txtPergjigjiaServeri.AppendText("\n" + Merr());
+            }
+            catch (SocketException se)
+            {
+                txtPergjigjiaServeri.AppendText("Nuk jeni lidhur me serverin. Ju lutem provoni përsëri!");
+                txtPergjigjiaServeri.AppendText("\n" + se.ToString());
+                return;
+            }
+
+            
+
+
+        }
+
+        private void Dergo(string teksti)
+        {
+            try
+            {
+                soketKlienti.Send(Encoding.UTF8.GetBytes(teksti));
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message.ToString());
+            }
+        }
+
+        public string Merr()
+        {
+            byte[] data = new byte[1024];
+            int receivedDataLength = soketKlienti.Receive(data);
+            string stringEdhena = Encoding.UTF8.GetString(data, 0, receivedDataLength);
+            return stringEdhena;
+            
+            
+        }
+        
+
+        private void BtnRegjistrohu_Click(object sender, EventArgs e)
+        {
+            string txtEmriMbiemriShifruar = DESShifrimi(txtEmriMbiemri.Text);
+            Dergo(txtEmriMbiemri.Text);
+            string txtTitulliShifruar = DESShifrimi(txtTitulli.Text);
+            Dergo(txtTitulli.Text);
+            string txtPagaShifruar = DESShifrimi(txtPaga.Text);
+            Dergo(txtPaga.Text);
+            string txtEPostaShifruar = DESShifrimi(txtEPosta.Text);
+            Dergo(txtEPosta.Text);
+            string txtNofkaShifruar = DESShifrimi(txtNofka.Text);
+            Dergo(txtNofka.Text);
+            string txtFjalkalimiShifruar = DESShifrimi(txtFjalkalimi.Text);
+            String salt = KrijoSalt(10);
+            string hashFjalkalimi = GjeneroSHA256Hashin(txtFjalkalimi.Text, salt);
+            
+            Dergo(hashFjalkalimi);
+        }
+    
+
+        private void BtnFutu_Click(object sender, EventArgs e)
+        {
+            string txtEmriMbiemriShifruar = DESShifrimi(txtEmriMbiemri.Text);
+            txtEmriMbiemri.Text = " ";
+            Dergo(txtEmriMbiemri.Text);
+            string txtTitulliShifruar = DESShifrimi(txtTitulli.Text);
+            txtTitulli.Text = " ";
+            Dergo(txtTitulli.Text);
+            string txtPagaShifruar = DESShifrimi(txtPaga.Text);
+            txtPaga.Text = " ";
+            Dergo(txtPaga.Text);
+            string txtEPostaShifruar = DESShifrimi(txtEPosta.Text);
+            txtEPosta.Text = " ";
+            Dergo(txtEPosta.Text);
+            string txtNofkaShifruar = DESShifrimi(txtNofka.Text);
+            Dergo(txtNofkaHyrja.Text);
+            string txtFjalkalimiShifruar = DESShifrimi(txtFjalkalimi.Text);
+            String salt = KrijoSalt(10);
+            string hashFjalkalimi = GjeneroSHA256Hashin(txtFjalkalimiHyrja.Text, salt);
+            Dergo(hashFjalkalimi);
+            
+            XmlDocument objXml = new XmlDocument();
+            RSACryptoServiceProvider objRsa = new RSACryptoServiceProvider();
+            //importimi i qelsit publik per te verifikuar nenshkrimin
+            
+            StreamReader sr = new StreamReader(@"/Users/Ndriqim/Desktop/Detyra2_Edi/Serveri/Serveri/bin/Debug/celesiPublik.xml");
+            string strXmlParametrat = sr.ReadToEnd();
+            sr.Close();
+
+            objRsa.FromXmlString(strXmlParametrat);
+
+            objXml.Load(@"/Users/Ndriqim/Desktop/Detyra2_Edi/Serveri/Serveri/bin/Debug/xmldb_nenshkruar.xml");
+            SignedXml objSignedXml = new SignedXml(objXml);
+
+            XmlNodeList signatureNodes = objXml.GetElementsByTagName("Signature");
+            XmlElement nenshkrimi = (XmlElement)signatureNodes[0];
+
+            // e lexon
+            objSignedXml.LoadXml(nenshkrimi);
+
+            if (objSignedXml.CheckSignature() == true)
+            {
+                // ne vend te console.writeline e bon pergjigja.appentText per me te dal te ai textboxi
+                txtPergjigjiaServeri.AppendText("Nenshkrimi valid!");
+                // nese eshte valid duhet me i shfaq te dhenat qka ka ne xml file
+                objXml.Load(@"/Users/Ndriqim/Desktop/Detyra2_Edi/Serveri/Serveri/bin/Debug/xmldb_nenshkruar.xml");
+                //txtPergjigjiaServeri.AppendText(objXml.ToString());
+
+            }
+            else
+            {
+                txtPergjigjiaServeri.AppendText("Nenshkrimi jo valid!");
+            }
+            
+        }
+
+        public static String KrijoSalt(int madhesia)
+        {
+
+            var nrRastesishem = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var baferi = new byte[madhesia];
+            nrRastesishem.GetBytes(baferi);
+            return Convert.ToBase64String(baferi);
+        }
+        public String GjeneroSHA256Hashin(String hyrja, String salt)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(hyrja + salt);
+            System.Security.Cryptography.SHA256Managed sha256hashstring =
+                new System.Security.Cryptography.SHA256Managed();
+            byte[] hash = sha256hashstring.ComputeHash(bytes);
+
+            return Convert.ToBase64String(hash);
+        }
+
+        private string DESShifrimi(string tekstiShifrues)
+        {
+            DESCryptoServiceProvider FjalaDES =
+                new DESCryptoServiceProvider();
+            FjalaDES.Key = Encoding.UTF8.GetBytes("12345678");
+            FjalaDES.IV = Encoding.UTF8.GetBytes("12345678");
+            FjalaDES.Padding = PaddingMode.Zeros;
+            FjalaDES.Mode = CipherMode.CBC;
+
+            byte[] bytePlaintext =
+                Encoding.UTF8.GetBytes(tekstiShifrues);
+            MemoryStream ms = new MemoryStream();
+
+            CryptoStream cs = new CryptoStream(ms,
+                                FjalaDES.CreateEncryptor(),
+                                CryptoStreamMode.Write);
+            cs.Write(bytePlaintext, 0, bytePlaintext.Length);
+            cs.Close();
+
+            byte[] byteCiphertexti = ms.ToArray();
+            return Convert.ToBase64String(byteCiphertexti);
+           // return Encoding.UTF8.GetString(byteCiphertexti);
+
+        }
+
+        private string DESDeshifrimi(string tekstiPerDeshifrim)
+        {
+            DESCryptoServiceProvider FjalaDES = new DESCryptoServiceProvider();
+            FjalaDES.Key = Encoding.UTF8.GetBytes("12345678");
+            FjalaDES.IV = Encoding.UTF8.GetBytes("12345678");
+            FjalaDES.Padding = PaddingMode.Zeros;
+            FjalaDES.Mode = CipherMode.CBC;
+
+            byte[] byteCiphertexti = Convert.FromBase64String(tekstiPerDeshifrim);
+            MemoryStream ms = new MemoryStream(byteCiphertexti);
+
+            byte[] byteTextiDeshifruar = new byte[ms.Length];
+            CryptoStream cs =
+                new CryptoStream(ms,
+                        FjalaDES.CreateDecryptor(),
+                        CryptoStreamMode.Read);
+            cs.Read(byteTextiDeshifruar, 0, byteTextiDeshifruar.Length);
+            cs.Close();
+
+            return Encoding.UTF8.GetString(byteTextiDeshifruar);
+        }
+
+       
+    }
+}
