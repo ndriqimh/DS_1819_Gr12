7d07c1130ae3bd33ca75ec062d57dfc6bf2d2895
Ndriqim Muhadri <44324826+NdriqimMuhadri@users.noreply.github.com>
2019-06-22 11:09:18 +0200

    Pjese të Klientit

---
+++ Klienti/Klienti/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+
+namespace Klienti
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormaUP());
+        }
+    }
+    /*
+    public class Mesimdhenesi
+    {
+        public string Nofka { get; set; }
+        public string HashFjalkalimi { get; set; }
+        public string EmriMbiemri { get; set; }
+        public string Titulli { get; set; }
+        public double Paga { get; set; }
+        public string EPosta { get; set; }
+    }
+    */
+}
